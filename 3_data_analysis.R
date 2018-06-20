69f795ba9c45ff3f8274f441e867dd195392d970
Inmaculada Barragan <inma_bv_93@hotmail.com>
2018-06-20 13:29:23 +0200

    First commit of all files

---
+++ 3_data_analysis.R
@@ -0,0 +1,7 @@
+#Analysis data
+#Installing Tidyverse
+install.packages("tidyverse")
+#Load the library to make available to scripts
+library("tidyverse")
+#Load the surveys data
+surveys <- read.csv("data/portal_data_joined.csv")
